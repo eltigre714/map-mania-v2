4c03f126d22dc14d2fe7968167e1a2d7675f3ff8
analyn pureza <analyn.c@hotmail.com>
2022-06-05 21:42:37 -0500

    updated app.jss, index.html and style.css

--- static/map-mania-ap/app.js
+++ static/map-mania-ap/app.js
@@ -1,1 +1,20 @@
-console.log("Hello World JS");
+// Initialize and add the map
+function initMap() {
+   
+    var mapOptions = {
+      zoom: 5,
+      center: {lat:41.7606, lng:-88.3201},
+    };
+
+    // New map
+    var newMap = new google.maps.Map(document.getElementById("map"), mapOptions);
+    }
+
+    // Add marker
+    var marker = new google.maps.Marker({
+        position:{lat:41.7606, lng:-88.3201},
+    map:newMap
+    });
+  
+
+
