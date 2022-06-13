e5560391e5ee5aa87e15df5d3530e548b8b24627
analyn pureza <analyn.c@hotmail.com>
2022-06-12 22:18:09 -0500

    added favorite places.

--- index.js
+++ index.js
@@ -120,6 +120,23 @@ app.get('/batman', (request, response) => {
 	response.send(JSON.stringify(spiderMan, null, 4))
 })
 
+var favePlaces  = [
+	{content: 'Aurora, IL', coordinates:{lat:41.7606, lng:-88.3201}},
+	{content: 'Universal Orlando Resort, FL', coordinates:{lat:28.3581, lng:-81.5591}},
+	{content: 'Chicago, IL', coordinates:{lat:41.8781, lng:-87.62981}},
+	{content: 'Florence, Italy', coordinates:{lat:43.7696, lng:11.2558}},
+	{content: 'Venice, Italy', coordinates:{lat:45.4408, lng:12.3155}},
+	{content: 'Kauai, HI', coordinates:{lat:22.0964, lng:-159.5261}},
+	{content: 'Disney World, Orlando, FL', coordinates:{lat:28.3772, lng:-81.5707}},
+	{content: 'Wisconsin Dells, WI', coordinates:{lat:43.6275, lng:-89.7710}},
+  ];
+
+  app.get('/favorite-places', (request, response) => {
+	console.log('Calling "/favorite-places" on the Node.js server.')
+	response.type('application/json')
+	response.send(JSON.stringify(favePlaces, null, 4))
+})
+
 // Custom 404 page.
 app.use((request, response) => {
   response.type('text/plain')
--- static/map-mania-ap/app.js
+++ static/map-mania-ap/app.js
@@ -15,6 +15,17 @@ function initMap() {
         position:{lat:41.7606, lng:-88.3201},
     map:newMap
     });
+
+    var favePlaces  = [
+      {content: 'Aurora, IL', coordinates:{lat:41.7606, lng:-88.3201}},
+      {content: 'Universal Orlando Resort, FL', coordinates:{lat:28.3581, lng:-81.5591}},
+      {content: 'Chicago, IL', coordinates:{lat:41.8781, lng:-87.62981}},
+      {content: 'Florence, Italy', coordinates:{lat:43.7696, lng:11.2558}},
+      {content: 'Venice, Italy', coordinates:{lat:45.4408, lng:12.3155}},
+      {content: 'Kauai, HI', coordinates:{lat:22.0964, lng:-159.5261}},
+      {content: 'Disney World, Orlando, FL', coordinates:{lat:28.3772, lng:-81.5707}},
+      {content: 'Wisconsin Dells, WI', coordinates:{lat:43.6275, lng:-89.7710}},
+    ];
   
 
 
